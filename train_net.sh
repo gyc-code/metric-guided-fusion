88d35bbe454399d7dcdfa18b188504cc869b6b62
misslibra <947177904@qq.com>
2024-10-22 18:37:55 +0200

    add shell

--- train_net.sh
+++ train_net.sh
@@ -4,28 +4,35 @@ configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_b
 
 # 8. train from coco and warm up together， s2t no filter 3k pixel, add random place to paste   
 # 这个实验最高结果在37.3， 由于学习率设置问题， 学习率一直在高水平，10-5 左右， 导致模型波动很大， 但是有机会冲到更高的值
-CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn >> ./output/inst/inst_urbansyn.txt
+CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn >> ./output/inst/inst_urbansyn.txt
 
 
 # train from coco and warm up together， s2t no filter 3k pixel, add random place to paste max_iter=30k lr=0.0001 power=0.9
-CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001 >> ./output/inst/inst_urbansyn_30klr0.0001.txt
+CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR 
+./output/inst/inst_urbansyn_30klr0.0001 >> ./output/inst/inst_urbansyn_30klr0.0001.txt
 
 # train from coco and warm up together， s2t no filter 3k pixel, add random place to paste max_iter=30k lr=0.0001 power=3 没有uda warmup
-CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_20klr0.0001_power3 >> ./output/inst/inst_urbansyn_20klr0.0001_power3.txt
+CUDA_VISIBLE_DEVICES=1,2 python train_net.py --num-gpus 2 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn_20klr0.0001_power3 >> ./output/inst/inst_urbansyn_20klr0.0001_power3.txt
 
 # train from coco and warm up together， s2t no filter 3k pixel, add random place to paste max_iter=30k lr=0.0001 power=3 warmup_iter=1000 uda_warmup=5000
 # use all instace for s2t and t2s, total_loss=0.5 s+0.25s2t+0.25t2s
-CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss.txt
+CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss.txt
 
 
 # train from coco and warm up together， s2t no filter 3k pixel, add random place to paste max_iter=30k lr=0.0001 power=3 warmup_iter=1000 uda_warmup=5000
 # use all instace for s2t and t2s, total_loss=0.5 s+0.25s2t+0.25t2s  fix ego car log bug, pseudo label>由0.9 改为 0.8
-CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss_1 >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss_1.txt
+CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss_1 >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_add_source_loss_1.txt
 
 
 # 1. use all instace for s2t and t2s, total_loss=mini batch loss,  fix ego car log bug, pseudo label>由0.9 改为 0.8
 # /home/yguo/Documents/other/UDA4Inst/debug_in_img_2024-05-27-18:07:45   伪标签多了一些错误  但是小目标更好了 mAP 到了36.3左右稳定。
-CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_minibatch_loss.txt
+CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_30klr0.0001_power3_allmix_minibatch_loss.txt
 
 # 2. same as upper. but use total_loss=0.5 s+0.25s2t+0.25t2s , to compare, bs = 1， 为了和实验1 比较,  mAP 到了36.3左右稳定, 在0.0001 poly power 2 下， 加权loss 和mini batch loss 基本相同
 # use all instace for s2t and t2s, total_loss=total_loss=0.5 s+0.25s2t+0.25t2s ,  fix ego car log bug, pseudo label>由0.9 改为 0.8
@@ -35,10 +42,91 @@ CUDA_VISIBLE_DEVICES=7 python train_net.py --num-gpus 1 --dist-url   tcp://127.0
 
 # 3. use all instace for s2t and t2s, total_loss=mini batch loss,  fix ego car log bug, pseudo label>由0.9 改为 0.8， lr 由0.0001提高到0.001， poly warmup 3000iter
 # 提高学习率，为了和1/ 8 比较， 主要修改了学习率设置。想让模型有机会冲到更高的点 不要太保守----结果完全飞了， 每1000轮测评都是0 ，warm up 无效。
-CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.001_power2_warmup3k_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_30klr0.001_power2_warmup3k_allmix_minibatch_loss.txt
+CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn_30klr0.001_power2_warmup3k_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_30klr0.001_power2_warmup3k_allmix_minibatch_loss.txt
 
 # 4. use all instace for s2t and t2s, total_loss=mini batch loss,  fix ego car log bug, pseudo label>由0.9 改为 0.8  lr 0.0001, no poly warm up, 試驗restart poly, 每2000輪一次，縂5k
-CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_50klr0.0001_power2_allmix_minibatch_loss_restart_poly >> ./output/inst/inst_urbansyn_50klr0.0001_power2_allmix_minibatch_loss_restart_poly.txt
+CUDA_VISIBLE_DEVICES=6 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn_50klr0.0001_power2_allmix_minibatch_loss_restart_poly >> ./output/inst/inst_urbansyn_50klr0.0001_power2_allmix_minibatch_loss_restart_poly.txt
 
 # 5. use all instace for s2t and t2s, total_loss=mini batch loss,  fix ego car log bug, pseudo label0.9 lr 0.0001, 900k iter, power0.9, to see if the changes can reach AP 37.3 again ?
-CUDA_VISIBLE_DEVICES=4 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/inst/inst_urbansyn_900klr0.0001_power0.9_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_900klr0.0001_power0.9_allmix_minibatch_loss.txt
+CUDA_VISIBLE_DEVICES=4 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/inst/inst_urbansyn_900klr0.0001_power0.9_allmix_minibatch_loss >> ./output/inst/inst_urbansyn_900klr0.0001_power0.9_allmix_minibatch_loss.txt
+
+#24/7/17 try far crop image ,45k,power=1,no warm,bs=3,45k for source ->45k, AP  all 0
+CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/only_source >> ./output/smartmix/only_source.txt
+
+# only source, colse far crop image replacement
+CUDA_VISIBLE_DEVICES=1,2,3 python train_net.py --num-gpus 3 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/only_source1 >> ./output/smartmix/only_source1.txt
+
+# test 0806
+CUDA_VISIBLE_DEVICES=0 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/only_source_test >> ./output/smartmix/only_source_test.txt
+
+CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/only_source_test_not_add_key >> ./output/smartmix/only_source_test_not_add_key.txt
+
+CUDA_VISIBLE_DEVICES=2 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_no_small >> ./output/smartmix/urbansyn_no_small.txt
+
+CUDA_VISIBLE_DEVICES=3 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml
+ OUTPUT_DIR ./output/smartmix/urbansyn_random_small >> ./output/smartmix/urbansyn_random_small.txt
+
+CUDA_VISIBLE_DEVICES=4 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml
+ OUTPUT_DIR ./output/smartmix/urbansyn_all_small >> ./output/smartmix/urbansyn_all_small.txt
+
+##  only source for 20k, total 45k
+CUDA_VISIBLE_DEVICES=2 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160
+ --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml
+  OUTPUT_DIR ./output/smartmix/urbansyn_random_small_fix_20kbs3 >> ./output/smartmix/urbansyn_random_small_fix_20kbs3.txt
+##  only source for 20k, total 45k
+CUDA_VISIBLE_DEVICES=3 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_no_small_bs3 >> ./output/smartmix/urbansyn_no_small_bs3.txt
+
+#################### icpr reject   20240821
+## do the exp again, first to get the performance with only source, from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3 total40k  (512, 1024)range(5, 21)
+CUDA_VISIBLE_DEVICES=0 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file 
+configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda_without.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_only_source >> ./output/smartmix/urbansyn_only_source.txt
+
+##  only source,but add far region crop random train. from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3 total40k   (512, 1024) range(5, 21)
+CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 
+--config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml
+ OUTPUT_DIR ./output/smartmix/urbansyn_only_source_far_region >> ./output/smartmix/urbansyn_only_source_far_region.txt
+
+##  only source,but add far region crop random train. from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3 total40k   (512, 1024) range(5, 10)
+CUDA_VISIBLE_DEVICES=3 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file 
+configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_only_source_far_region_range_5_10 >> ./output/smartmix/urbansyn_only_source_far_region_range_5_10.txt
+
+##  only source,but add far region crop random train. from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3 total40k   (1024, 1024) range(5, 10)
+CUDA_VISIBLE_DEVICES=4 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/smartmix/urbansyn_only_source_far_region_range_5_10_crop1024 >> ./output/smartmix/urbansyn_only_source_far_region_range_5_10_crop1024.txt
+
+## do the exp again, first to get the performance with only source, from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3  total40k (512, 1024)  range(5, 10)
+CUDA_VISIBLE_DEVICES=0 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file 
+configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda_without.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_only_source_range_5_10 >> ./output/smartmix/urbansyn_only_source_range_5_10.txt
+
+
+## do the exp again, first to get the performance with only source, from coco, base lr0.001, warm up,0.5,1500. MAX_SIZE_TRAIN: 4096, bs = 3 total40k (1024, 1024)  range(5, 10)
+CUDA_VISIBLE_DEVICES=1 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file 
+configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda_without.yaml 
+OUTPUT_DIR ./output/smartmix/urbansyn_only_source_range_5_10_crop1024 >> ./output/smartmix/urbansyn_only_source_range_5_10_crop1024.txt
+
+#  基于这个last model : urbansyn_only_source_range_5_10 ,开始训练。参数一样。使用了两阶段的伪标签生成，第一阶段是随机裁剪，第二阶段是随机裁剪+far region crop.
+# 在far region 处有重复instance  MINI_BATCH_LOSS
+# error in eval
+CUDA_VISIBLE_DEVICES=0 python train_net.py --num-gpus 1 --dist-url   tcp://127.0.0.1:50160 --config-file configs/cityscapes/instance-segmentation/swin/maskformer2_swin_large_IN21k_384_bs16_90k_uda.yaml OUTPUT_DIR ./output/smartmix/urbansyn_UDA_start_from_lastmodel_far_crop >> ./output/smartmix/urbansyn_UDA_start_from_lastmodel_far_crop.txt
