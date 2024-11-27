8fd75d6cfefa8df216ecdefdf54624e7db02e1f3
misslibra <947177904@qq.com>
2024-11-27 22:13:18 +0100

    correct instance eval

--- detectron2/detectron2/evaluation/cityscapes_evaluation.py
+++ detectron2/detectron2/evaluation/cityscapes_evaluation.py
@@ -179,8 +179,8 @@ class CityscapesInstanceEvaluator(CityscapesEvaluator):
             )["averages"]
 
             ret = OrderedDict()
-            # ret["segm"] = {"AP": results["allAp"] * 100, "AP50": results["allAp50%"] * 100}
-            ret["segm"] = {"AP": results["allAp"] * 100, "AP50": results["allAp50%"] * 100, "AP50m": results["allAp50m"] * 100, 
+            ret["segm"] = {"AP": results["allAp"] * 100, "AP50": results["allAp50%"] * 100}
+            # ret["segm"] = {"AP": results["allAp"] * 100, "AP50": results["allAp50%"] * 100, "AP50m": results["allAp50m"] * 100, 
                            "AP100m": results["allAp100m"] * 100, "AP50%50m": results["allAp50%50m"] * 100}
             self._logger.info(ret)
             self._working_dir.cleanup() 
